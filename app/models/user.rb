05befd0d892f83138c08033be9326abe060e473d
kakiti <kakizaki3315chi@gmail.com>
2020-12-03 18:27:46 +0900

    バリデーション#修正3

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
     
   with_options presence: true do
     validates :nickname
-    with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ } do
+    with_options format: { with: /\A[ぁ-んァ-ヶ一-龥]+\z/ } do
       validates :last_name
       validates :first_name
     end

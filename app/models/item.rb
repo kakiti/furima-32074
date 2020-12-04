bab571e7674e7af3f574cf62ad388bd7d66448d8
kakiti <kakizaki3315chi@gmail.com>
2020-12-04 18:20:15 +0900

    Itemモデルとuserモデルの編集

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,31 @@
 class Item < ApplicationRecord
+
+  with_options presence: true do
+    validates :image
+    validates :title
+    validates :concept
+    validates :price
+  end
+  with_options numericality: {other_than: 1 } do
+    validates :category_id
+    validates :status_id
+    validates :fee_id
+    validates :prefecture_id
+    validates :schedule_id 
+  end
+
+
+  belongs_to :user
+  has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :status
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :fee
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :prefecture
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :schedule
+
 end

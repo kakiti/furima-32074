80a9d14bec37b98f2a893e5ec5d645f73b9ce539
kakiti <kakizaki3315chi@gmail.com>
2020-12-05 15:04:04 +0900

    itemscontroller修正２

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :move_to_index, expect: :index
+  before_action :move_to_index, only: [:new, :create]
 
   def index
   end
@@ -24,6 +24,6 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    redirect_to action: :index unless user_signed_in?
+    redirect_to new_user_session_path unless user_signed_in?
   end
 end

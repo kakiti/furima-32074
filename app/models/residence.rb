9edd871505d0dbfb8953f1ed6ed8b99036b1d72d
kakiti <kakizaki3315chi@gmail.com>
2020-12-08 18:58:25 +0900

    バリデーションの記述

--- app/models/residence.rb
+++ app/models/residence.rb
@@ -1,3 +1,11 @@
 class Residence < ApplicationRecord
+  with_options presence: true do
+    validates :postal_code
+    validates :prefecture_id, numericality: { other_than: 1 } do
+    validates :city
+    validates :address
+    validates :phone_number
+  end
+
   belongs_to :order
 end

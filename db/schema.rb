63bef764bdc0dc2588b8501b5d1a7819b530bd51
kakiti <kakizaki3315chi@gmail.com>
2020-12-08 18:57:37 +0900

    ordersテーブル、residencesテーブル作成#修正

--- db/schema.rb
+++ db/schema.rb
@@ -62,7 +62,7 @@ ActiveRecord::Schema.define(version: 2020_12_08_093801) do
     t.integer "prefecture_id", null: false
     t.string "city", null: false
     t.string "address", null: false
-    t.string "building", null: false
+    t.string "building"
     t.string "phone_number", null: false
     t.bigint "order_id"
     t.datetime "created_at", precision: 6, null: false
